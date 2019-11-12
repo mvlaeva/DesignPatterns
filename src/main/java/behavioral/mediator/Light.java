9e05058baa8310a5b7102819ff1d63a412c3059a
mirela <mim_0011@abv.bg>
2019-11-12 19:22:30 +0200

    behavioral patterns

---
+++ src/main/java/behavioral/mediator/Light.java
@@ -0,0 +1,34 @@
+package behavioral.mediator;
+
+//receiver
+public class Light {
+
+    private boolean isOn = false;
+    private String location = "";
+
+    public Light(String location){
+        this.location = location;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void toggle() {
+        if (isOn) {
+            off();
+            isOn = false;
+        } else {
+            on();
+            isOn = true;
+        }
+    }
+
+    private void on() {
+        System.out.println("Light switched on.");
+    }
+
+    private void off() {
+        System.out.println("Light switched off.");
+    }
+}
