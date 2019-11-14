d60927627b129aba3b033e78cba9d7b9fb66fce8
Mirela <mirela_vlaeva@epam.com>
2019-11-14 17:55:22 +0200

    behavioral patterns

---
+++ src/main/java/behavioral/strategy/StrategyDemo.java
@@ -0,0 +1,29 @@
+package behavioral.strategy;
+
+public class StrategyDemo {
+
+    public static void main(String[] args) {
+        CreditCard amexCard = new CreditCard(new AmexStrategy());
+
+        amexCard.setNumber("1234123412341234");
+        amexCard.setDate("04/2020");
+        amexCard.setCvv("123");
+
+        System.out.println("Is Amex valid: " + amexCard.isValid());
+
+        amexCard.setNumber("379185883464283");
+        amexCard.setDate("04/2020");
+        amexCard.setCvv("123");
+
+        System.out.println("Is Amex valid: " + amexCard.isValid());
+
+        CreditCard visaCard = new CreditCard(new VisaStrategy());
+
+        visaCard.setNumber("4539589763663402");
+        visaCard.setDate("05/2018");
+        visaCard.setCvv("324");
+
+        System.out.println("Is Visa valid: " + visaCard.isValid());
+
+    }
+}
---
+++ src/main/java/behavioral/templatemethodpattern/TemplateMethodPatternDemo.java
@@ -0,0 +1,15 @@
+package behavioral.templatemethodpattern;
+
+public class TemplateMethodPatternDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Web Order: ");
+
+        OrderTemplate webOrder = new WebOrder();
+        webOrder.processOrder();
+
+        System.out.println("\nStore Order: ");
+        OrderTemplate storeOrder = new StoreOrder();
+        storeOrder.processOrder();
+    }
+}
